169abc57273a2227002e5df95cc5c2a3f34f5d96
jeevavighnesh <jeevavighnesh@gmail.com>
2017-01-23 10:52:44 +0530

    added scripts to the testing

---
+++ scripts/mysql_cloud_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "call pr_order_food ('c1',2,'South Indian Meals,North Indian Meals,Variety Rice','2,1,1');"|mysql -udev_user -pdev_pwd -h139.59.29.57 ashwini_db
---
+++ scripts/mysql_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "use Soru; CALL PR_SUBORDER_SPLITTER('COFFEE 4,IDLY 10,POORI 7,TEA 5,VADA 5',1);"|mysql -uroot -pquarter#9112
